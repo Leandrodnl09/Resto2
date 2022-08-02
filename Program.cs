6eb645f4dc4b355f140fed860d7f442ab57c2314
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-02 15:32:41 -0300

    Resto2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Exercício do site URI "Resto!"
+// Esse programa o usuário ira digitar om valor inteiro "N", e em seguida mostrará na tela quais são divisivel pelo numero "N" de 1 até 1000.
+
+
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10000; i++)
+{
+    if (i % N == 2)
+    {
+        Console.WriteLine(i);
+    }
+}
